26db49fd0c5e3bf08693317d3c21296e6d857844
Castelli <isisdare@gmail.com>
2020-03-14 00:43:41 -0300

    Referenciando e retornando os posts de um usuário

--- src/main/java/com/castelli/workshopmongo/domain/Post.java
+++ src/main/java/com/castelli/workshopmongo/domain/Post.java
@@ -2,9 +2,9 @@ package com.castelli.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import com.castelli.workshopmongo.dto.AuthorDTO;
 
 @Document
 public class Post implements Serializable {
@@ -15,12 +15,12 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -61,11 +61,11 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 	
