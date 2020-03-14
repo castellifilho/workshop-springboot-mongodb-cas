5bb5ad15d392d29b0096aaf7fb038c34dc8949d4
Castelli <isisdare@gmail.com>
2020-03-13 23:38:45 -0300

    Post com User aninhado

--- src/main/java/com/castelli/workshopmongo/resources/UserResource.java
+++ src/main/java/com/castelli/workshopmongo/resources/UserResource.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.castelli.workshopmongo.domain.User;
 import com.castelli.workshopmongo.dto.UserDTO;
 import com.castelli.workshopmongo.services.UserService;
@@ -47,6 +46,14 @@ public class UserResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
