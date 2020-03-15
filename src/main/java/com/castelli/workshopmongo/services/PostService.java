629f67a84c09bb6038d74f2f93bebb3f0d10a758
Castelli <isisdare@gmail.com>
2020-03-15 18:42:52 -0300

    Consultas Query

--- src/main/java/com/castelli/workshopmongo/services/PostService.java
+++ src/main/java/com/castelli/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.castelli.workshopmongo.services;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +21,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
